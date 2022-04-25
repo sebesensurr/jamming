402fa6f2deed7d6054802f41ae1b29b00716827b
sebesensurr <nagyfero@gmail.com>
2022-04-25 14:28:44 +0100

    test

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { TrackList } from '../TrackList/TrackList';
+import './Playlist.css'
+
+export class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChangeName = this.handleChangeName.bind(this);
+    }
+
+    handleChangeName(event) {
+        this.props.onNameChange(event.target.value);
+    } 
+    
+    render() {
+        return (
+            <div className="Playlist">
+                <input value={this.props.playlistName} onChange={this.handleChangeName} />
+                <TrackList tracks={this.props.playlistTracks}
+                            onRemove={this.props.onRemove}
+                            isRemoval={true} />
+                <button className="Playlist-save"
+                        onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+            </div>
+        );
+    }
+}
